ba09faa2fe6e13d65443b428282b2d750809025f
mikanmatsuri <natsufuji83m@gmail.com>
2020-08-12 19:45:55 +0900

    #17 ハンバーガーアイコンga

--- js/main.js
+++ js/main.js
@@ -1,41 +1,56 @@
 // ページがダウンロードされるのを待つ
 window.onload = function(){
 
-    //ページの「予約メール」欄以外のものがクリックされたら、
-    window.document.getElementById("reservation-mail").onblur = function(){
-        // ユーザーのメールアドレスを取得する
-        var email = document.getElementById("reservation-mail").value;
-        //　メールアドレスがメールアドレスの形が間違っているかを取得する
-        var result = email.match(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/);
-        //もしも、メールアドレスの形が間違っていたら
-        if(result === null){
-            //　予約エラーを表示する
-            document.getElementById("reservation-mail-error").style.display = "block";
-        }else{
-            // 間違っていなかったら、予約エラーを表示しない
-            document.getElementById("reservation-mail-error").style.display = "none";
+    /*やっている処理は下のハンバーガーの処理と同じ。こちらの方が読みやすいし管理しやすいらしい
+     //-----------------------------------------------------------------------------
+    //-------------------------ハンバーガーが押された時の処理-------------------------
+    //-----------------------------------------------------------------------------
+    document.getElementById("smartphone-menu-button").onclick = function () {
+        //メニューの表示がされてなかったら、表示する
+        var menu = document.getElementById("smartphone-menu-list");
+        if (menu.style.display === "none"){
+            menu.style.display = "block";
+        }
+        //メニューの表示がされてたら、表示しない
+        else if (menu.style.display === "block"){
+            menu.style.display = "none";
+        }
+    };
+    */
+
+    //-----------------------------------------------------------------------------
+    //-------------------------ハンバーガーが押された時の処理-------------------------
+    //-----------------------------------------------------------------------------
+    var menuButton = document.getElementById("smartphone-menu-button");
+    //メニューが表示されていないことを定義する
+    var menuDisplayFlag = false;
+    document.getElementById("smartphone-menu-button").onclick = function () {
+        /*メニューの表示がされてなかったら、表示する*/
+        if (menuDisplayFlag === false){
+            document.getElementById("smartphone-menu-list").style.display = "block";
+            menuDisplayFlag = true;
+        }
+        //メニューの表示がされてたら、表示しない
+        else if (menuDisplayFlag === true){
+            document.getElementById("smartphone-menu-list").style.display = "none";
+            menuDisplayFlag = false;
+        }
+
+        //メニューのリストの中の一つの項目をクリックするとメニューが非表示になる
+        var linkCount = document.getElementsByClassName("js-menu-button-click").length;
+        for(var i = 0; i < linkCount; i++) {
+            document.getElementsByClassName("js-menu-button-click")[i].onclick = function () {
+                //smartphone-menu-listを非表示にする
+                console.log(1);
+                document.getElementById("smartphone-menu-list").style.display = "none";
+            };
         }
     };
 
-    //ページの「お名前」欄以外のものがクリックされたら、
-    window.document.getElementById("reservation-name").onblur = function(){
-        // ユーザーの名前を取得する
-        var name = document.getElementById("reservation-name").value;
-        //　名前の長さを計算する
-        var result = name.length;
-        //もしも、名前が10文字以上なら
-        if(result > 10){
-            //　予約エラーを表示する
-            document.getElementById("reservation-name-error").style.display = "block";
-        }else{
-            // 間違っていなかったら、予約エラーを表示しない
-            document.getElementById("reservation-name-error").style.display = "none";
-        }
-    };
+    //-----------------------------------------------------------------------------
+    //--------------------------------言語切替の処理--------------------------------
+    //-----------------------------------------------------------------------------
 };
-
-
-
 
 
 
